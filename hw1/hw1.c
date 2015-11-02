49c7f358d6974dd93e038cf1ec5957b5596436df
Takeshi <a86487817@gmail.com>
2015-11-02 20:06:09 +0800

    Improve multithread performance
    1. move points_in_circle outside the thread
    2. use rand_r instead of rand to generate random number
    3. remove unused prompt

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -5,30 +5,33 @@
 #include <math.h>
 #include <sys/sysinfo.h>
 
-
+unsigned long long* points_in_circle;
+int total_points = 0;
+int num_thread = 1;
 
 void* cal_thread(void* arg) {
-    int np = *((int*) arg);
+    int tid = *((int*) arg);
+    int *seed = malloc(sizeof(int));
+    *seed = ((int)time(NULL) ^ tid);
     int c;
-    unsigned long long* point_in_circle = malloc(sizeof(unsigned long long));
-    *point_in_circle = 0;
+    int np = total_points / num_thread;
+    points_in_circle[tid] = 0;
     for(c=0; c<np; c++) {
-        double x = (double)rand() / RAND_MAX;
-        double y = (double)rand() / RAND_MAX;
+        double x = (double)rand_r(arg) / RAND_MAX;
+        double y = (double)rand_r(arg) / RAND_MAX;
         double r2 = x*x + y*y;
         if(r2 <= 1) {
-            *point_in_circle = *point_in_circle + 1;
+            ++points_in_circle[tid];
         }
     }
-    return (void*) point_in_circle;
+    return NULL;
 }
 
 int main(int argc, const char *argv[])
 {
-    int num_thread = get_nprocs();
-    int total_points = 0;
     int c;
     int tmp = 0;
+    num_thread = get_nprocs();
     if(argc < 2) {
         printf("usage: ./hw1 number-of-tosses\n");
         return -1;
@@ -39,24 +42,24 @@ int main(int argc, const char *argv[])
         printf("num of tosses should be larger than 0");
         return -1;
     }
+
     pthread_t* thread = malloc(sizeof(pthread_t) * num_thread);
-    srand(time(NULL));
+    points_in_circle = malloc(sizeof(unsigned long long) * num_thread);
 
     for(c=0; c<num_thread; c++) {
         int* arg = malloc(sizeof(int));
-        *arg = total_points / num_thread;
-        if(c == num_thread - 1) {
-            *arg = total_points - tmp;
-        }
-        tmp += *arg;
+        *arg = c; // tid
+        tmp += total_points / num_thread;
         pthread_create(&thread[c], NULL, cal_thread, arg);
     }
+    total_points = tmp;
+
     unsigned long long total_point_in_circle = 0;
     for(c=0; c<num_thread; c++) {
         void* retval;
         pthread_join(thread[c], &retval);
-        total_point_in_circle += *((unsigned long long*)retval);
+        total_point_in_circle += points_in_circle[c];
     }
-    printf("PI : %f\n", (double)total_point_in_circle*4 / total_points);
+    printf("%.15f\n", (double)total_point_in_circle*4 / total_points);
     return 0;
 }
